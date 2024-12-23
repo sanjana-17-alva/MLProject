495829fbb7da53ca06c64925bfd1d19f87cf04d1
Sanjana Alva <sanjanadjalva@gmail.com>
2024-12-23 21:27:43 +0530

    Changes in logs

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -98,7 +98,8 @@ class ModelTrainer:
             # the threshold can be varied
             if best_model_score < 0.6:
                 raise CustomException("No best model found")
-            logging.info(f"Best found model on both training and testing dataset")
+            
+            logging.info(f"Best found model: {best_model_name}")
 
             save_object(
                 file_path=self.model_trainer_config.trained_model_file_path,
--- src/logger.py
+++ src/logger.py
@@ -1,3 +1,31 @@
+# import logging  # Import the logging module to handle logging messages
+# import os  
+# from datetime import datetime  
+
+# # Create a log file name based on the current date and time in the format MM_DD_YYYY_HH_MM_SS.log
+# LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+# # Define the path for the logs folder and create the path for the log file
+# logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
+
+# # Create the 'logs' directory if it doesn't already exist (exist_ok=True prevents errors if the directory exists)
+# os.makedirs(logs_path, exist_ok=True)
+
+# # Define the full path for the log file, combining the logs directory path with the log file name
+# LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+
+# # Configure logging: setting the log file path, log format, and log level (INFO level and above will be logged)
+# logging.basicConfig(
+#     filename=LOG_FILE_PATH,  
+#     format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",  # Define the format for log messages
+#     level=logging.INFO  # Set the logging level to INFO (logs messages with level INFO and above)
+# )
+
+# # only to check if the logger code is running 
+# # if __name__ == "__main__":
+# #     logging.info("Logging has started")
+
+
 import logging  # Import the logging module to handle logging messages
 import os  
 from datetime import datetime  
@@ -5,8 +33,8 @@ from datetime import datetime
 # Create a log file name based on the current date and time in the format MM_DD_YYYY_HH_MM_SS.log
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
-# Define the path for the logs folder and create the path for the log file
-logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
+# Define the path for the logs folder
+logs_path = os.path.join(os.getcwd(), "logs")
 
 # Create the 'logs' directory if it doesn't already exist (exist_ok=True prevents errors if the directory exists)
 os.makedirs(logs_path, exist_ok=True)
@@ -21,6 +49,5 @@ logging.basicConfig(
     level=logging.INFO  # Set the logging level to INFO (logs messages with level INFO and above)
 )
 
-# only to check if the logger code is running 
-# if __name__ == "__main__":
-#     logging.info("Logging has started")
+# Example log to confirm the logger is working
+logging.info("Logging has started")
